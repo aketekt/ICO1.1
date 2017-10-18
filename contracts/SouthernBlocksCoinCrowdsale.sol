b5d92cafaa82e093a6e5134cb90bab233eaa8386
aketekt <will.sch@protonmail.com>
2017-10-18 21:59:50 +0100

    add price change

--- contracts/SouthernBlocksCoinCrowdsale.sol
+++ contracts/SouthernBlocksCoinCrowdsale.sol
@@ -1,19 +1,99 @@
-pragma solidity ^0.4.15;
+pragma solidity ^0.4.11;
 
-import './SouthernBlocksCoin.sol';
-import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
-
+import "zeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
+import "./SouthernBlocksCoin.sol";
 
 contract SouthernBlocksCoinCrowdsale is Crowdsale {
+    
 
-  function SouthernBlocksCoinCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) 
-    Crowdsale(_startTime, _endTime, _rate, _wallet) {          
-  }
+    // customize the rate for each whitelisted buyer
+    mapping (address => uint256) public buyerRate;
 
-  // creates the token to be sold.
-  // override this method to have crowdsale of a specific MintableToken token.
-  function createTokenContract() internal returns (MintableToken) {
-    return new SouthernBlocksCoin();
-  }
+    // intial rate at which tokens are offered
+    uint256 public intialRate;
+
+    // end rate at which tokens are offered
+    uint256 public endRate;
+
+    // continuous crowdsale contract
+   
+    event WalletChange(address wallet);
+
+    event intialRateChange(uint256 rate);
+
+    event EndRateChange(uint256 rate);
+
+    function SouthernBlocksCoinCrowdsale(
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _intialRate,
+        uint256 _endRate,
+        address _wallet
+    )
+     
+        Crowdsale(_startTime, _endTime, _intialRate, _wallet)
+    {
+        require(_intialRate > 0);
+        require(_endRate > 0);
+
+        intialRate = _intialRate;
+        endRate = _endRate;
+    }
+
+    function createTokenContract() internal returns(MintableToken) {
+        return new SouthernBlocksCoin();
+    }
+
+
+    function setBuyerRate(address buyer, uint256 rate) public {
+        require(rate != 0);
+        require(block.number < startTime);
+
+        buyerRate[buyer] = rate;
+    }
+
+    function setintialRate(uint256 rate) public {
+        require(rate != 0);
+        require(block.number < startTime);
+
+        intialRate = rate;
+
+        intialRateChange(rate);
+    }
+
+    function setEndRate(uint256 rate) public {
+        require(rate != 0);
+        require(block.number < startTime);
+
+        endRate = rate;
+
+        EndRateChange(rate);
+    }
+
+    function getRate() internal returns(uint256) {
+        // some early buyers are offered a discount on the crowdsale price
+        if (buyerRate[msg.sender] != 0) {
+            return buyerRate[msg.sender];
+        }
+
+        // otherwise compute the price for the auction
+        uint256 elapsed = block.number - startTime;
+        uint256 rateRange = intialRate - endRate;
+        uint256 blockRange = endTime - startTime;
+
+        return intialRate.sub(rateRange.mul(elapsed).div(blockRange));
+    }
+
+    // low level token purchase function
+    function buyTokens(address beneficiary) payable {
+        require(beneficiary != 0x0);
+        require(validPurchase());
+
+        uint256 weiAmount = msg.value;
+        uint256 updatedWeiRaised = weiRaised.add(weiAmount);
+
+        uint256 rate = getRate();
+    }
+
 
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,10 @@ const SouthernBlocksCoinCrowdsale = artifacts.require("./SouthernBlocksCoinCrowd
 module.exports = function(deployer, network, accounts) {
   const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 30 // one second in the future
   const endTime = startTime + (86400 * 20) // 20 days
-  const rate = new web3.BigNumber(1000)
+  const intialRate = new web3.BigNumber(1000)
+  const endRate = new web3.BigNumber(500)
   const wallet = accounts[0]
+ 
 
-  deployer.deploy(SouthernBlocksCoinCrowdsale, startTime, endTime, rate, wallet)
+  deployer.deploy(SouthernBlocksCoinCrowdsale, startTime, endTime, intialRate, endRate, wallet)
 };
